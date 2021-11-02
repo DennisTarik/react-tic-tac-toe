a14407a23bb9d0fbc3759505e302d4771094a0c7
DennisTarik <dm499555@gmail.com>
2021-11-02 16:53:04 +0100

    done

--- src/app/components/Square.tsx
+++ src/app/components/Square.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import { prependOnceListener } from 'process';
+import React, { useState } from 'react';
 
-function Square(): JSX.Element {
-  return <button className="square">{/* TODO */}</button>;
+function Square(props): JSX.Element {
+
+  return  (<button
+             className="square"
+             onClick={() => props.onClick('X')}
+             >
+            {props.value}
+          </button>
+          );
 }
 
 export default Square;
